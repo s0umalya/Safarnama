3477e28c04eafafbc29f3ac51b42fb90e2308356
Soumalya Chakraborty <soumalya.chk@gmail.com>
2026-01-22 15:15:20 +0530

    Logic for the admins to view booked trips

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,5 +1,26 @@
+const Booking = require('../models/booking.model');
+
 exports.createTripPackage = (req, res) => {
   res.status(201).json({
     message: 'Trip package created (admin only)'
   });
+};
+
+exports.getAllBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find()
+      .sort({ createdAt: -1 })
+      .populate('user', 'firstName lastName email')
+      .select(
+        'tripSnapshot travelDate numberOfPeople priceSnapshot status createdAt user'
+      );
+
+    res.status(200).json(bookings);
+
+  } catch (error) {
+    console.error('Admin Get Bookings Error:', error);
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
 };
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 const { authenticate } = require('../middleware/auth.middleware');
 const { authorizeRoles } = require('../middleware/role.middleware');
-const { createTripPackage } = require('../controllers/admin.controller');
+const { createTripPackage, getAllBookings  } = require('../controllers/admin.controller');
 
 router.post(
   '/create-packages',
   authenticate,
   authorizeRoles('ADMIN'),
   createTripPackage
+);
+
+router.get(
+  '/bookings',
+  authenticate,
+  authorizeRoles('ADMIN'),
+  getAllBookings
 );
 
 module.exports = router;
