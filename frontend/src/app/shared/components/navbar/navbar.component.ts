dbd48388d4d16c2247617d2663e74fc89f42f26d
Soumalya Chakraborty <soumalya.chk@gmail.com>
2026-02-01 02:18:28 +0530

    Basic mobile responsiveness of navbar

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   standalone: true,
@@ -9,9 +12,21 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
     RouterLink
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update(open => !open);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+}
