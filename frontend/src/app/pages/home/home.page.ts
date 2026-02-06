6ea852cd4d48a70e2a1c2992c357ef8ea7b93854
Soumalya Chakraborty <soumalya.chk@gmail.com>
2026-02-07 00:32:41 +0530

    Hero section base

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './home.page.html',
-  styleUrl: './home.page.scss'
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage {}
